0e533aed2845f5c311397fe4df78c89e022583d3
Alessia Bellisario <bellisario.alessia@gmail.com>
2016-02-20 11:30:48 -0500

    added server logic to saved custom fields added first and last name

--- futurenowteam.com.js
+++ futurenowteam.com.js
@@ -3,11 +3,15 @@ if (Meteor.isClient) {
   Template.register.events({
     'submit form': function (event) {
       event.preventDefault();
+      var first_name = $('[name=first_name]').val();
+      var last_name = $('[name=last_name]').val();
       var email = $('[name=email]').val();
       var password = $('[name=password]').val();
       var type = $('input:checked').val();
 
       Accounts.createUser({
+        first_name: first_name,
+        last_name: last_name,
         email: email,
         password: password,
         type: type
@@ -18,6 +22,14 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    Accounts.onCreateUser(function(options, user) {
+      user.first_name = options.first_name;
+      user.last_name = options.last_name;
+      user.type = options.type;
+      user.is_admin = false;
+      return user;
+
+    })
     // code to run on server at startup
   });
 }
